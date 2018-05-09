acb84ea55264870553cad1c7967823316452630d
kopalina351 <39101313+kopalina351@users.noreply.github.com>
2018-05-09 13:34:16 +0300

    Update CalculatorDemo.java

--- src/main/java/Module3/CalculatorDemo.java
+++ src/main/java/Module3/CalculatorDemo.java
@@ -5,7 +5,7 @@ public class CalculatorDemo {
 
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
-		calculator.add(4, 2);
+		calculator.add(6, 2);
 		calculator.mPlus(calculator.add(5, 2), 1);
 		calculator.mClear(calculator.mPlus(calculator.add(5, 2), 1));
 
